f8a86187503f614c8932b52e2275a1d79c89878b
Tien Dao <tiendao5240@gmail.com>
2022-12-02 15:29:34 -0500

    SwiftUIContinueLearning: first commit

---
+++ SwiftUIContinueLearning/SwiftUIContinueLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinueLearningApp.swift
+//  SwiftUIContinueLearning
+//
+//  Created by Tien Dao on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinueLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
